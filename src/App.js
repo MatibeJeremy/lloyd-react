fa0e8566fcdb6a2f5f46892f22d1b63b23dfbc09
Jeremy <matibejeremy@gmail.com>
2021-06-27 17:05:27 +0300

    Sunday push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Header from './Components/Header';
+import Body from './Components/Body';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [],
+            search: '',
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    componentDidMount() {
+        axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=4f49aa8`)
+            .then(res => {
+                const movies = res.data;
+                this.setState({movies});
+                console.log(this.state.movies)
+            })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        alert('Sorry, there was only one record in the response. Hence search could not apply');
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Header
+                    movies={this.state.movies}
+                />
+                <form className="d-flex" style={{padding: 20}}>
+                    <input className="form-control me-2" type="search" placeholder="Search"
+                           aria-label="Search"/>
+                    <button onClick={this.handleSubmit} className="btn btn-outline-success"
+                            type="submit">Search
+                    </button>
+                </form>
+                <Body
+                    src={this.state.movies.Poster}
+                    title={this.state.movies.Title}
+                    production={this.state.movies.Production}
+                    plot={this.state.movies.Plot}
+                    rated={this.state.movies.Rated}
+                    release={this.state.movies.Released}
+                />
+
+            </div>
+        );
+    }
 }
 
 export default App;
